0aaf1e4da1d861dd6861dfb3e188b933868d8258
SleepySlime <liuqing.suzy@gmail.com>
2023-04-22 12:18:44 -0700

    [preCP] added content preview TODO@SSLM fix mwp_menu.png after correction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import marley_main from './mwp_main.png';
+import mwp_menu from './mwp_menu.png';
+import mwp_content from './mwp_content.png';
 import './App.css';
 
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <div className="image-container">
-        <img src={marley_main} alt="background" className="scrollable-image" />
+        <img src={mwp_menu} alt="mwp_main" className="scrollable-image" />
+        <img src={mwp_content} alt="mwp_menu" className="scrollable-image" />
       </div>
     </div>
   );
