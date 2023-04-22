84e99cdccaee42fc1fc1a4cb4a2f0a23ab2221e6
SleepySlime <liuqing.suzy@gmail.com>
2023-04-22 11:28:41 -0700

    [preCP] scrollable image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import marley_main from './mwp_main.png';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="image-container">
+        <img src={marley_main} alt="background" className="scrollable-image" />
+      </div>
     </div>
   );
 }
